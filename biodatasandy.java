c003cad24a7e6c741fac9a17c95d4f2f54a246d2
sandyy221 <kurniawansandy808.com>
2024-09-05 12:03:47 +0700

    tugas kelompok sandy

---
+++ biodatasandy.java
@@ -0,0 +1,16 @@
+public class biodatasandy {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Sandy Kurniawan");
+        System.out.println("NIM     : 244107020029");
+        System.out.println("Kelas   : 1D");
+        System.out.println("No Absen: 28");
+        System.out.println("Prodi   : D4-Teknik Informatika");
+        System.out.println("Alamat  : Kec.Semen, Kab.Kediri, Jawa Timur");
+        System.out.println("No HP   : 085704950052");
+        System.out.println("Hobi    : Basket & Game");
+
+    }
+}
